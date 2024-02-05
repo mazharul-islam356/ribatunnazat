d9efab1ce751c678e07f772098a550983f8d18c1
rifat3568 <mazharulislam3569@gmail.com>
2024-02-05 20:08:55 +0600

    added ibrahim info

--- src/Components/member/Member.jsx
+++ src/Components/member/Member.jsx
@@ -1,13 +1,6 @@
-import { useState } from "react";
-import {
-  Dialog,
-} from "@material-tailwind/react";
 import './member.css'
 
 const Member = () => {
-  const [open, setOpen] = useState(false);
- 
-  const handleOpen = () => setOpen(!open);
 
 
     return (  
@@ -19,7 +12,7 @@ const Member = () => {
 
 {/* 01 */}
 				<tr className="bg-teal-400 flex flex-col flex-no wrap sm:table-row rounded-l-lg sm:rounded-none mb-2 sm:mb-0">
-					<th className="p-3 text-left">Id</th>
+					<th className="p-3 text-left pl-6">Id</th>
 					<th className="p-3 text-left">Name</th>
 					<th className="p-3 text-left lg:h-0 h-[4.2rem]">Image</th>
 					<th className="p-3 text-left lg:pl-40">Position</th>
@@ -51,7 +44,7 @@ const Member = () => {
 		<tbody className="flex-1 sm:flex-none">
       {/* 01 jahid */}
 				<tr className="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0">
-					<td className="w-40 ml-10 lg:ml-0 p-3">2022001</td>
+					<td className="w-40 ml-10 lg:ml-0 p-3 pl-6">2022001</td>
 					<td className="lg:w-80 ml-10 lg:ml-0 p-3">Jahidul Islam</td>
           <td className="w-16 p-3 ml-10 lg:ml-0">  <div>
     <img className="rounded-xl h-24 w-20" src="https://i.ibb.co/yNp4zFW/Whats-App-Image-2024-01-12-at-8-51-30-PM.jpg" />
@@ -64,7 +57,7 @@ const Member = () => {
 <dialog id="my_modal_1" className="modal">
 
 
-  <div className="modal-box">
+  <div className="modal-box"> 
 
   <div className="avatar flex justify-center mt-4">
           
@@ -102,23 +95,14 @@ const Member = () => {
     <button>close</button>
   </form>
 
-
-
-
-
 </dialog>
 </td>
+		</tr>
 
 
-
-				
-
-						
-      
-				</tr>
       {/* 02 hamed */}
 				<tr className="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0">
-					<td className="w-40 ml-10 lg:ml-0 p-3">2022002</td>
+					<td className="w-40 ml-10 lg:ml-0 p-3 pl-6">2022002</td>
 					<td className="lg:w-80 ml-10 lg:ml-0 p-3">Hamed</td>
           <td className="w-16 p-3 ml-10 lg:ml-0">  <div>
     <img className="rounded-xl h-24 w-20" src="https://i.ibb.co/qMWSZh3/Whats-App-Image-2024-01-12-at-8-51-34-PM.jpg" />
@@ -183,65 +167,66 @@ const Member = () => {
 						
       
 				</tr>
-
-
-       
-
-
-
-
-        {/* 03 ibrahim */}
+      {/* 03 ibrahim */}
 				<tr className="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0">
-					<td className="w-40 ml-10 lg:ml-0 p-3">2022003</td>
+					<td className="w-40 ml-10 lg:ml-0 p-3 pl-6">2022003</td>
 					<td className="lg:w-80 ml-10 lg:ml-0 p-3">Ibrahim</td>
           <td className="w-16 p-3 ml-10 lg:ml-0">  <div>
-    <img className="rounded-xl" src="" />
+    <img className="rounded-xl h-24 w-20" src="https://i.ibb.co/KVjnR2y/Whats-App-Image-2024-01-12-at-8-48-21-PM.jpg" />
   </div></td>
   <td className="p-3 w-40 lg:pl-40 ml-10 lg:ml-0">অর্থ-সম্পাদক</td>
-					<td className=" lg:mt-14">
-						<button  className="btn btn-accent btn-sm btn-outline ml-10 lg:ml-0">Details</button>
 
-						<Dialog
-        open={open}
-        handler={handleOpen}
-        animate={{
-          mount: { scale: 1, y: 0 },
-          unmount: { scale: 0.9, y: -100 },
-        }}
-      >
-        <div className="avatar flex justify-center mt-4">
-  <div className="w-28 rounded-xl">
-    <img src="" />
-  </div>
-  </div>
+<td>
 
-  <div className="mt-10 mb-6">
+<button className="btn btn-accent btn-sm btn-outline ml-10 lg:ml-0" onClick={()=>document.getElementById('my_modal_3').showModal()}>Details</button>
+<dialog id="my_modal_3" className="modal">
 
-  <div className="flex gap-1 justify-center mt-2">
-  <span className="font-bold text-xl">নাম:</span> <div className="italic">মুহা: জাহিদুল ইসলাম</div>
-  </div>
-  <div className="flex gap-1 justify-center mt-1">
-  <span className="font-bold text-xl">পিতার নাম:</span> <div className="italic">মো: মুক্তার আহমাদ</div>
-  </div>
-  <div className="flex gap-1 justify-center mt-1">
-  <span className="font-bold text-xl">মাতার নাম:</span> <div className="italic">মোসা: ছেনোয়ারা বেগম</div>
-  </div>
-  <div className="flex gap-1 justify-center mt-1">
-  <span className="font-bold text-xl">ঠিকানা:</span> <div className="italic">পূর্ব বোমংখিল, গর্জনিয়া, রামু, কক্সবাজার
-</div>
-  </div>
-  <div className="flex gap-1 justify-center mt-1">
-  <span className="font-bold text-xl">মোবাইল:</span> <div className="italic">০১৮৮১৭৩৯৮৫৫
-</div>
-  </div>
-
-
-  </div>
-       
+  <div className="modal-box">
+  <div className="avatar flex justify-center mt-4">
+          
+          <div className="w-28 rounded-xl">
+            <img src="https://i.ibb.co/KVjnR2y/Whats-App-Image-2024-01-12-at-8-48-21-PM.jpg" />
+          </div>
+          </div>
         
-      </Dialog>
-					</td>
-				</tr>
+          <div className="mt-10 mb-6">
+        
+          <div className="flex gap-1 justify-center mt-2">
+          <span className="font-bold text-xl">নাম:</span> <div className="italic">Ibrahim</div>
+          </div>
+          <div className="flex gap-1 justify-center mt-1">
+          <span className="font-bold text-xl">পিতার নাম:</span> <div className="italic">মো: মুক্তার আহমাদ</div>
+          </div>
+          <div className="flex gap-1 justify-center mt-1">
+          <span className="font-bold text-xl">মাতার নাম:</span> <div className="italic">মোসা: ছেনোয়ারা বেগম</div>
+          </div>
+          <div className="flex gap-1 justify-center mt-1">
+          <span className="font-bold text-xl">ঠিকানা:</span> <div className="italic">পূর্ব বোমংখিল, গর্জনিয়া, রামু, কক্সবাজার
+        </div>
+          </div>
+          <div className="flex gap-1 justify-center mt-1">
+          <span className="font-bold text-xl">মোবাইল:</span> <div className="italic">০১৮৮১৭৩৯৮৫৫
+        </div>
+          </div>
+          </div>
+
+
+  </div>
+ 
+
+  <form method="dialog" className="modal-backdrop">
+    <button>close</button>
+  </form>
+
+
+
+
+
+</dialog>
+</td>
+  
+	</tr>
+
 
 
                
